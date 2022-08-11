5e5b0d14b6efe05044cfe4bd1e6c36ca32ea1928
Erdem Siyam <erdemsiyam@gmail.com>
2022-08-11 15:32:30 +0300

    personel listeleme

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,10 +5,9 @@ sap.ui.define(
     function (Controller) {
         "use strict";
         return Controller.extend(
-            "staff.App",
+            "staff.controller.App",
             {
                 onInit: function () {
-                    
                 }
             }
         );
